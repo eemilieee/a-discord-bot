0003d9ff14fecf35419ac199365aad6df6345fda
eemilieee <emyarpa@gmail.com>
2021-12-07 20:16:57 +0200

    added the python code for the bot

---
+++ py-bot.py
@@ -0,0 +1,250 @@
+#!./.venv/bin/python
+ 
+import discord      # base discord module
+import code         # code.interact
+import os           # environment variables
+import inspect      # call stack inspection
+import random       # dumb random number generator
+
+import youtube_dl
+from discord.ext import commands,tasks    # Bot class and utils
+from dotenv import load_dotenv
+
+ 
+################################################################################
+############################### HELPER FUNCTIONS ###############################
+################################################################################
+ 
+# log_msg - fancy print
+#   @msg   : string to print
+#   @level : log level from {'debug', 'info', 'warning', 'error'}
+def log_msg(msg: str, level: str):
+    # user selectable display config (prompt symbol, color)
+    dsp_sel = {
+        'debug'   : ('\033[34m', '-'),
+        'info'    : ('\033[32m', '*'),
+        'warning' : ('\033[33m', '?'),
+        'error'   : ('\033[31m', '!'),
+    }
+ 
+    # internal ansi codes
+    _extra_ansi = {
+        'critical' : '\033[35m',
+        'bold'     : '\033[1m',
+        'unbold'   : '\033[2m',
+        'clear'    : '\033[0m',
+    }
+ 
+    # get information about call site
+    caller = inspect.stack()[1]
+ 
+    # input sanity check
+    if level not in dsp_sel:
+        print('%s%s[@] %s:%d %sBad log level: "%s"%s' % \
+            (_extra_ansi['critical'], _extra_ansi['bold'],
+             caller.function, caller.lineno,
+             _extra_ansi['unbold'], level, _extra_ansi['clear']))
+        return
+ 
+    # print the damn message already
+    print('%s%s[%s] %s:%d %s%s%s' % \
+        (_extra_ansi['bold'], *dsp_sel[level],
+         caller.function, caller.lineno,
+         _extra_ansi['unbold'], msg, _extra_ansi['clear']))
+ 
+################################################################################
+############################## BOT IMPLEMENTATION ##############################
+################################################################################
+ 
+# bot instantiation
+bot = commands.Bot(command_prefix='!')
+ 
+# on_ready - called after connection to server is established
+@bot.event
+async def on_ready():
+    log_msg('logged on as <%s>' % bot.user, 'info')
+ 
+# on_message - called when a new message is posted to the server
+#   @msg : discord.message.Message
+@bot.event
+async def on_message(msg):
+    # filter out our own messages
+    if msg.author == bot.user:
+        return
+ 
+    log_msg('message from <%s>: "%s"' % (msg.author, msg.content), 'debug')
+ 
+    # overriding the default on_message handler blocks commands from executing
+    # manually call the bot's command processor on given message
+    await bot.process_commands(msg)
+ 
+# roll - rng chat command
+#   @ctx     : command invocation context
+#   @max_val : upper bound for number generation (must be at least 1)
+@bot.command(brief='Generate random number between 1 and <arg>')
+async def roll(ctx, max_val: int):
+    # argument sanity check
+    if max_val < 1:
+        raise Exception('argument <max_val> must be at least 1')
+ 
+    await ctx.send(random.randint(1, max_val))
+ 
+# roll_error - error handler for the <roll> command
+#   @ctx     : command that crashed invocation context
+#   @error   : ...
+@roll.error
+async def roll_error(ctx, error):
+    await ctx.send(str(error))
+
+
+
+load_dotenv()
+intents = discord.Intents().all()
+client = discord.Client(intents=intents)
+
+youtube_dl.utils.bug_reports_message = lambda: ''
+
+ytdl_format_options = {
+    'format': 'bestaudio/best',
+    'restrictfilenames': True,
+    'noplaylist': True,
+    'nocheckcertificate': True,
+    'ignoreerrors': False,
+    'logtostderr': False,
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'auto',
+    'source_address': '0.0.0.0' # bind to ipv4 since ipv6 addresses cause issues sometimes
+}
+
+ffmpeg_options = {
+    'options': '-vn'
+}
+
+ytdl = youtube_dl.YoutubeDL(ytdl_format_options)
+
+class YTDLSource(discord.PCMVolumeTransformer):
+    def __init__(self, source, *, data, volume=0.5):
+        super().__init__(source, volume)
+        self.data = data
+        self.title = data.get('title')
+        self.url = ""
+
+    @classmethod
+    async def from_url(cls, url, *, loop=None, stream=False):
+        loop = loop or asyncio.get_event_loop()
+        data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
+        if 'entries' in data:
+            # take first item from a playlist
+            data = data['entries'][0]
+        filename = data['title'] if stream else ytdl.prepare_filename(data)
+        return filename
+
+
+@bot.command(name='play', help='To play a desired song; you simply need the URL off Youtube :3')
+async def play(ctx,url):
+    if not ctx.message.author.name=="emilieee" :
+         await ctx.send('Nice try, you have to be my master :>')
+         return
+    try :
+        server = ctx.message.guild
+        voice_channel = server.voice_client
+
+        async with ctx.typing():
+            filename = await YTDLSource.from_url(url, loop=bot.loop)
+            voice_channel.play(discord.FFmpegPCMAudio(source=filename))
+        await ctx.send('**You are now listening to:** {}'.format(filename))
+    except:
+        await ctx.send("The bot is not connected to a voice channel. ;(((")
+
+@bot.command(name='join', help='Tells the bot to join the voice channel')
+async def join(ctx):
+    if not ctx.message.author.voice:
+        await ctx.send("{} is not connected to a voice channel".format(ctx.message.author.name))
+        return
+    else:
+        channel = ctx.message.author.voice.channel
+    await channel.connect()
+
+
+@bot.command(name='pause', help='This command pauses the song at the desired moment')
+async def pause(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.pause()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+    
+@bot.command(name='resume', help='Resumes the song from the last moment it stopped playing')
+async def resume(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_paused():
+        await voice_client.resume()
+    else:
+        await ctx.send("The bot was not playing anything before this. Use the **play** command to start listening to something :)")
+    
+@bot.command(name='scram', help='To make the bot leave the voice channel :((')
+async def scram(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_connected():
+        await voice_client.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+@bot.command(name='stop', help='Stops the song that is currently playing')
+async def stop(ctx):
+    voice_client = ctx.message.guild.voice_client
+    if voice_client.is_playing():
+        await voice_client.stop()
+    else:
+        await ctx.send("The bot is not playing anything at the moment.")
+
+
+@bot.event
+async def on_ready():
+    print('Running!')
+    for guild in bot.guilds:
+        for channel in guild.text_channels :
+            if str(channel) == "general" :
+                await channel.send('I am alive now! But that is a sad thing in a cruel world :((')
+                await channel.send(file=discord.File('giphy.png'))
+
+@bot.command()
+async def me(ctx):
+    text = "I am the saddest bot alive :(( and at present I have an only mission: to cure your depression"
+    await ctx.send(text)
+
+@bot.event
+async def on_message(message) :
+    # bot.process_commands(msg) is a couroutine that must be called here since we are overriding the on_message event
+    await bot.process_commands(message) 
+    if str(message.content).lower() == "hello":
+        await message.channel.send('Hi!')
+        
+@bot.event
+async def on_voice_state_update(member, before, after):
+    voice_state = member.guild.voice_client
+    if voice_state is None:
+        return 
+    if len(voice_state.channel.members) == 1:
+        await voice_state.disconnect()
+
+#@bot.command(name='command list', help='To show a list of song stored in the memory')
+#async def list(ctx):
+#	for file in os.listdir("./"):
+#		if file.endswith(".wav"):
+#			await ctx.send(file)
+
+################################################################################
+############################# PROGRAM ENTRY POINT ##############################
+################################################################################
+ 
+if __name__ == '__main__':
+    # check that token exists in environment
+    if 'BOT_TOKEN' not in os.environ:
+        log_msg('save your token in the BOT_TOKEN env variable!', 'error')
+        exit(-1)
+ 
+    # launch bot (blocking operation)
+    bot.run(os.environ['BOT_TOKEN'])
+code.interact(local=dict(globals(), **locals())) 
